acc01793183fb4ef3d515795a3d8dadbf2694fd9
Phan Truong Du <s3270226@student.rmit.edu.au>
2012-08-27 21:48:04 +1000

    fix searchView to use pdo

--- searchView.php
+++ searchView.php
@@ -18,53 +18,53 @@
 		<input type="text" name="wineryName">
 		<?php
 		//region select
-			echo '<br>Region:';
-			$query = "SELECT * FROM region";
-			$result = mysql_query($query, $dbconn);
-			echo '<select name="Region">';
-			while($row=mysql_fetch_array($result))
-			{
+			echo '<br>Region:';		
+			$sql="SELECT * FROM region";
+			echo '<select name="region">';
+			$statement=$db->prepare($sql);
+			$statement->execute();
+			foreach($statement->fetchAll(PDO::FETCH_ASSOC) as $row){
 				echo '<option value="' . htmlspecialchars($row['region_name']) . '">' 
 				. $row['region_name'] . '</option>';
 			}
 			echo '</select>';
 		
 		//grape variety select
-			echo '<br>Grape Variety:';
-			$query = "SELECT * FROM grape_variety";
-			$result = mysql_query($query, $dbconn);
+			echo '<br>Grape Variety:';		
+			$sql="SELECT * FROM grape_variety";
 			echo '<select name="grapeVariety">';
 			echo '<Option value="All">All</Option>';
-			while($row=mysql_fetch_array($result))
-			{
+			$statement=$db->prepare($sql);
+			$statement->execute();
+			foreach($statement->fetchAll(PDO::FETCH_ASSOC) as $row){
 				echo '<option value="' . htmlspecialchars($row['variety']) . '">' 
 				. $row['variety'] . '</option>';
 			}
 			echo '</select>';
-
-		//year select
+			
 			echo '<br>Year';
 			echo '<br>Lower bound:';
-			$query = "SELECT year FROM wine  group by year order by year";
-			$result = mysql_query($query, $dbconn);
+			$sql="SELECT year FROM wine GROUP BY year ORDER BY year";
 			echo '<select name="yearLowerBound">';
-			while($row=mysql_fetch_array($result))
-			{
-				echo '<option value="' . htmlspecialchars($row['year']) . '">' 
-				. $row['year'] . '</option>';
+			$statement=$db->prepare($sql);
+			$statement->execute();
+			foreach($statement->fetchAll(PDO::FETCH_ASSOC) as $row){
+				echo '<option value="'.htmlspecialchars($row['year']).'">'
+				.$row['year'].'</option>';
 			}
 			echo '</select>';
 			
-			$query = "SELECT year FROM wine group by year order by year desc";
-			$result = mysql_query($query, $dbconn);
 			echo 'Upper bound:';
+			$sql="SELECT year FROM wine GROUP BY year ORDER BY year DESC";
 			echo '<select name="yearUpperBound">';
-			while($row=mysql_fetch_array($result))
-			{
-				echo '<option value="' . htmlspecialchars($row['year']) . '">' 
-				. $row['year'] . '</option>';
+			$statement=$db->prepare($sql);
+			$statement->execute();
+			foreach($statement->fetchAll(PDO::FETCH_ASSOC) as $row){
+				echo '<option value="'.htmlspecialchars($row['year']).'">'
+				.$row['year'].'</option>';
 			}
 			echo '</select>';
+			$db=null;
 		?>
 		<br>Minimum number of wine in stock:
 		<input type="text" name="minimumNumberOfWineInStock">
