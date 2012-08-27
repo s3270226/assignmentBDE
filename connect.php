63db863228d5affb92cc481e45c99f0e7d9925d4
Phan Truong Du <s3270226@student.rmit.edu.au>
2012-08-27 21:31:48 +1000

    change connect file to use pdo

--- connect.php
+++ connect.php
@@ -1,18 +1,9 @@
 <?php
-/**
-*Checks the connection to the local install of MySQL
-*/
-require_once('db.php');
-if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
-   echo 'Could not connect to mysql on ' . DB_HOST . "\n";
-   exit;
-}
-//echo 'Connected to mysql on '. DB_HOST . "\n";
-if(!mysql_select_db(DB_NAME, $dbconn)){
-   echo 'Could not use database ' . DB_NAME . "\n";
-   echo mysql_error() . "\n";
-   exit;
-}
-//echo 'Connected to database ' . DB_NAME . "\n";
+require_once('config.ini.php');
+	try{
+		$db=new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME,DB_USER,DB_PW);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(PDOException $e){
+		echo $e->getMessage();
+	}
 ?>
-
